26f4be017fcad07faa2ab3cf87ae0dbf7d46f174
KristofferGW <kristoffer.wallqvist@medieinstitutet.se>
2024-06-11 17:46:17 +0200

    Refactored basic tests

--- test/SimpleErc20TokenWithAirdrop.t.sol
+++ test/SimpleErc20TokenWithAirdrop.t.sol
@@ -8,24 +8,29 @@ import {DeploySimpleErc20TokenWithAirdrop} from "../script/DeploySimpleErc20Toke
 contract SimpleErc20TokenWithAirdropTest is Test {
     SimpleErc20TokenWithAirdrop simpleErc20TokenWithAirdrop;
 
+    string constant TOKEN_NAME = "BonesCoin";
+    string constant TOKEN_SYMBOL = "$BONES";
+    uint256 constant TOTAL_SUPPLY = 666666666;
+    address constant INITIAL_SUPPLY_HOLDER = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
+
     function setUp() external {
         DeploySimpleErc20TokenWithAirdrop deploySimpleErc20TokenWithAirdrop = new DeploySimpleErc20TokenWithAirdrop();
         simpleErc20TokenWithAirdrop = deploySimpleErc20TokenWithAirdrop.run();
     }
 
     function testTokenName() public view {
-        assertEq(simpleErc20TokenWithAirdrop.name(), "BonesCoin");
+        assertEq(simpleErc20TokenWithAirdrop.name(), TOKEN_NAME);
     }
 
     function testTokenSymbol() public view {
-        assertEq(simpleErc20TokenWithAirdrop.symbol(), "$BONES");
+        assertEq(simpleErc20TokenWithAirdrop.symbol(), TOKEN_SYMBOL);
     }
 
     function testTotalSupply() public view {
-        assertEq(simpleErc20TokenWithAirdrop.totalSupply(), 666666666);
+        assertEq(simpleErc20TokenWithAirdrop.totalSupply(), TOTAL_SUPPLY);
     }
 
     function testInitialSupplyHolder() public view {
-        assertEq(simpleErc20TokenWithAirdrop.balanceOf(0x70997970C51812dc3A010C7d01b50e0d17dc79C8), 666666666);
+        assertEq(simpleErc20TokenWithAirdrop.balanceOf(INITIAL_SUPPLY_HOLDER), TOTAL_SUPPLY);
     }
 }
